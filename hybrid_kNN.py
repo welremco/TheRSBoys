7af06180d745a9302e42810b7944604ced1668b7
MichielBloemdeijk <sander@odcamp.nl>
2021-11-29 17:29:15 +0100

    update collaborative filtering

--- hybrid_kNN.py
+++ hybrid_kNN.py
@@ -1,46 +1,91 @@
 import load_data
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
 
 
-def cosine_similarity_array(movie_ratings, index):
+def cosine_similarity_array(movie_ratings, index, movie_ratings_nonzero):
     """
     Calculate cosine similarity between movie at index and all other movies in movie_ratings array
     :param movie_ratings: 2d array of size (num_movies, num_users)
     :param index: index of movie to compare against all other movies
+    :param movie_ratings_nonzero: list of indices of nonzero movies in movie_ratings
     :return: 1d array with cosine similarities of each movie with the movie at index (including with itself)
     """
     similarity = np.zeros(len(movie_ratings))
     movie_i_ratings = movie_ratings[index]
-    ri = np.nonzero(movie_i_ratings)
-    for j in range(len(movie_ratings)):
-        movie_j_ratings = movie_ratings[j]
-        rj = np.nonzero(movie_j_ratings)
-        ids = np.intersect1d(ri, rj)
-        movie_i_nonzero_ratings = np.take(movie_i_ratings, ids)
-        movie_j_nonzero_ratings = np.take(movie_j_ratings, ids)
-        if len(movie_j_nonzero_ratings) > 0:
-            similarity[j] = cosine_similarity(np.expand_dims(movie_i_nonzero_ratings, 0),
-                                              np.expand_dims(movie_j_nonzero_ratings, 0))[0][0]
-        else:
-            similarity[j] = 0
+    ri = movie_ratings_nonzero[index]
+    # TODO tune amount of users that need to have rated a movie
+    if len(ri) > 100:
+        for j in range(len(movie_ratings)):
+            movie_j_ratings = movie_ratings[j]
+            rj = movie_ratings_nonzero[j]
+            # TODO tune amount of users that need to have rated a movie
+            if len(rj) > 100:
+                ids = np.intersect1d(ri, rj, assume_unique=True)
+                # TODO tune amount of users that need to have rated both movies
+                if len(ids) > 10:
+                    movie_i_nonzero_ratings = movie_i_ratings[ids]
+                    movie_j_nonzero_ratings = movie_j_ratings[ids]
+                    similarity[j] = faster_cosine_similarity(movie_i_nonzero_ratings, movie_j_nonzero_ratings)
+
     return similarity
 
-print("loading movie data...")
-train_movie_genres, val_movie_genres, train_ratings, val_ratings, movie_names = load_data.load_data("MovieLens_m1/movies.dat", "MovieLens_m1/ratings.dat")
 
-# take out movies with fewer than 10 ratings
-# TODO tune amount of ratings
-train_ratings = np.where(np.sum(train_ratings != 0, axis=0) >= 1000, train_ratings, 0)
+def faster_cosine_similarity(a, b):
+    return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
 
-while True:
-    id = int(input("movie id: "))
-    print(movie_names[id])
 
-    similarity_array = cosine_similarity_array(np.transpose(train_ratings), id)
-    similar_movie_ids = np.argpartition(similarity_array, -5)[-5:]
-    similar_movie_ids_score = similarity_array[similar_movie_ids]
+def load_movie_data():
+    print("loading movie data...")
+    return load_data.load_data("MovieLens_m1/movies.dat", "MovieLens_m1/ratings.dat")
 
-    for i in range(len(similar_movie_ids)):
-        similar_id = similar_movie_ids[i]
-        print(movie_names[similar_id], similar_movie_ids_score[i])
+
+def load_similarity_matrix(calculate=False, train_ratings=None):
+    """
+    Load similarity matrix from file or calculate it
+    :param calculate: whether to calculate the data
+    :param train_ratings: 2d array of size (num_movies, num_users), only used if calculate is True
+    :return: similarity matrix
+    """
+    if calculate:
+        print("preprocessing...")
+        movie_ratings_nonzero = []
+        train_ratings_t = np.transpose(train_ratings)
+        for i in range(len(train_ratings_t)):
+            movie_ratings_nonzero.append(np.nonzero(train_ratings_t[i])[0])
+
+        print("preprocessing matrix...")
+        similarity_matrix = np.zeros((len(train_ratings_t), len(train_ratings_t)), dtype=np.float16)
+        for i in range(len(train_ratings_t)):
+            print(i)
+            similarity_matrix[i] = cosine_similarity_array(train_ratings_t, i, movie_ratings_nonzero)
+
+        np.save("similarity_matrix.npy", similarity_matrix)
+    else:
+        similarity_matrix = np.load("similarity_matrix.npy")
+    return similarity_matrix
+
+
+def get_cf_results(similarity_matrix, ratings, movie_names, user_id):
+    """
+    Get the cosine similarity scores for a user based on their watched movies
+    :param similarity_matrix: precomputed matrix of cosine similarities between movies
+    :param ratings: 2d array of size (num_movies, num_users)
+    :param movie_names: list of movie names
+    :param user_id: user id to get recommendations for
+    :return: sorted list of tuples of form (movie_name, predicted_rating)
+    """
+    recommendations = {}
+    for movie_id in range(len(ratings[:, user_id])):
+        rating = ratings[movie_id, user_id]
+        if rating != 0 and movie_id in movie_names:
+            similarity_array = similarity_matrix[movie_id]
+            for i in range(len(similarity_array)):
+                if similarity_array[i] != 0:
+                    # TODO determine aggregation method for similarity scores
+                    # now just adds them together
+                    if movie_names[i] not in recommendations:
+                        recommendations[movie_names[i]] = similarity_array[i]
+                    else:
+                        recommendations[movie_names[i]] += similarity_array[i]
+    print({k: v for k, v in sorted(recommendations.items(), key=lambda item: item[1])})
+    return {k: v for k, v in sorted(recommendations.items(), key=lambda item: item[1], reverse=True)}
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from load_data import load_data
 import os
+import hybrid_kNN as hk
+import numpy as np
 
 dirname = os.path.dirname(__file__)
 movies_file = os.path.join(dirname, 'MovieLens_m1/movies.dat')
 ratings_file = os.path.join(dirname, 'MovieLens_m1/ratings.dat')
 
-# a,b,c=load_data_movies(movies_file)
-# d,e,f=load_data_ratings(ratings_file)
-a,b,c,d = load_data(movies_file, ratings_file)
-# print(a)
-# print(b)
-print(c)
-print(d)
+print("Loading movie data...")
+train_movie_genres, valid_movie_genres, train_movie_ratings, valid_movie_ratings, movie_names = load_data(movies_file,
+                                                                                                          ratings_file)
+
+print("Loading precomputed similarity matrix...")
+similarity_matrix = hk.load_similarity_matrix()
+while True:
+    user_id = int(input("Enter user id: "))
+    scores_dict = hk.get_cf_results(similarity_matrix, np.transpose(train_movie_ratings), movie_names, user_id)
+    print(scores_dict)
